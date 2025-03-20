22f8599ebfab4eaec1df9f37e64b117dcd6f130a
GantaSriKavya <srikavya1612@gmail.com>
2025-03-20 22:25:11 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,26 @@
+package srikavya;
+import java.net.MulticastSocket;
+public class pattern3 
+{
+	public static void main(String[] args) 
+	{
+		 for(int i=1;i<=5;i++)
+	      {
+	    	  for(int j=5;j>=i;j--)
+	    	  {
+	    		  System.out.print(" ");
+	    	  }
+	    	   for(int k=1;k<(i*2);k++)
+	    	   {
+	    		   System.out.print("*");
+	    	   }
+	    	  System.out.println(" ");
+	      }
+	}
+}
+
+/* output :   *
+             ***
+            *****
+           *******
+          *********    */
