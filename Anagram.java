3b6f800493bf3a4368f5bb8dfa13413d6c5fbde8
GantaSriKavya <srikavya1612@gmail.com>
2025-03-26 10:29:52 +0530

    Add files via upload

---
+++ Anagram.java
@@ -0,0 +1,19 @@
+package javaproject;
+
+public class Anagram {
+
+	public static void main(String[] args) 
+	{
+       String s1="keep";
+       String s2="peek";
+       if(s1.length()==s2.length())
+       {
+    	   System.out.println(" is a anagram");
+       }
+       else
+       {
+    	   System.out.println("is not a anagram");
+       }
+	}
+
+}
