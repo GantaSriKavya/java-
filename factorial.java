ac71a8f2aa81c7a3b0587b1f8073b2b86f602a0e
GantaSriKavya <srikavya1612@gmail.com>
2025-04-01 15:17:25 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,22 @@
+package javaproject;
+import java.util.*;
+public class factorial 
+{
+	public static void main(String[] args) 
+	{
+      int fact=1;
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter the number :");
+      int n=sc.nextInt();
+      for(int i=1;i<=n;i++)
+      {
+    	  fact=fact*i;
+      }
+      System.out.println("Factorial of "+n+" is "+ fact);
+	}
+}
+
+
+/*  output :  Enter the number :
+              4
+              Factorial of 4 is 24  */
